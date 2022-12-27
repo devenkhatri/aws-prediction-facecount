24db857722ba62d4c6825242afafde5915b46971
Deven Goratela <deven.goratela@tcs.com>
2022-12-27 12:27:15 +0530

    prediction added

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,11 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "predictions": {
+        "identifyLabelsfcea0662": {
+            "region": "string",
+            "type": "string"
+        }
     }
 }
--- src/App.js
+++ src/App.js
@@ -1,27 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react'
-import { Storage } from 'aws-amplify';
+import { Storage, Predictions } from 'aws-amplify'
 
 function App() {
   const [file, setFile] = useState();
   const [uploaded, setUploaded] = useState(false);
+  const [hasFace, setHasFace] = useState(false);
   return (
     <div className="App">
       <input type="file" onChange={(e) => setFile(e.target.files[0])} />
       <button onClick={async () => {
-        const storageResult = await Storage.put('faces.png', file, {
+        const storageResult = await Storage.put(file.name, file, {
           level: 'public',
-          type: 'image/png'
+          type: file.type
         })
-        // Insert predictions code here later
+        console.log("******** file", file)
         setUploaded(true)
         console.log(storageResult);
+        setHasFace(false)
+        await Predictions.identify({
+          labels: {
+            source: {
+              file,
+            },
+            type: "LABELS"
+          }
+        })
+          .then(response => {
+            const { labels } = response;
+            labels.forEach(object => {
+              const { name } = object
+              console.log("****** name", name)
+              if(name === 'Face') setHasFace(true)
+          });
+          })
+          .catch(err => console.log({ err }));
       }}>Upload and count the number of faces in an image!</button>
       <div>
         {uploaded
           ? <div>Your image is uploaded!</div>
           : <div>Upload a photo to get started</div>}
+        {hasFace
+          ? <div> This image contains a face</div>
+          : <div> This image doesn't have a face </div>}
       </div>
     </div>
   );
